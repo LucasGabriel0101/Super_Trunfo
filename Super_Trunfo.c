d384cdfcd235326ad5c03a6380d88bbeac2fd02d
Lucas Gabriel Diniz <lglucas30@gmail.com>
2025-10-13 23:49:25 -0300

    Add initial project files and basic card data
    Added .vscode/settings.json to disable C/C++ error squiggles. Created Super_Trunfo.c with initial structure and variables for two cards representing cities, including population, area, and GDP data.

---
+++ Super_Trunfo.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(){
+
+//      CARTA 01       //
+
+char Estado1 = A;
+char CódigodaCarta1 = ("A01");
+char NomedaCidade1 = ("São Paulo");
+int População1 = 12325000;
+float Área1 = 1521.11;
+float PIB1 = 699.28;
+int NúmeroPontos1 = 50
+
+char Estado2 = B;
+char CódigodaCarta2 = ("B02");
+char NomedaCidade2 = ("Rio de Janeiro");
+int População2 = 6748000;
+float Área2 = 1200.25;
+float PIB2 = 300.50;
+int NúmeroPontos2 = 30
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+ 
