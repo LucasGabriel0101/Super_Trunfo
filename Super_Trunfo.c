96cb58121ac50c4b045e6d4eb465b7189804d9e4
Lucas Gabriel Diniz <lglucas30@gmail.com>
2025-10-14 01:42:53 -0300

    Super_Trunfo.c

--- Super_Trunfo.c
+++ Super_Trunfo.c
@@ -1,30 +1,45 @@
 #include <stdio.h>
 
 int main(){
+   char Estado1 = 'A';
+   char Codigo1[] = "A01";
+   char Cidade1[] = "Sao Paulo";
+   int Populacao1 = 12325000;
+   float Area1 =  1521.11;
+   float PIB1 = 699.28;
+   int Pontos1 = 50;
 
-//      CARTA 01       //
+   char Estado2 = 'B';
+   char Codigo2[] = "B02";
+   char Cidade2[] = "Rio de Janeiro";
+   int Populacao2 = 6748000;
+   float Area2 =  1200.25;
+   float PIB2 = 300.50;
+   int Pontos2 = 30;
 
-char Estado1 = A;
-char CódigodaCarta1 = ("A01");
-char NomedaCidade1 = ("São Paulo");
-int População1 = 12325000;
-float Área1 = 1521.11;
-float PIB1 = 699.28;
-int NúmeroPontos1 = 50
+printf("---------------------------------------\n");
+   printf("Carta 1:\n");
+   printf("Estado: %c\n", Estado1); 
+   printf("Codigo: %s\n", Codigo1);
+   printf("Nome da Cidade: %s\n", Cidade1);
+   printf("Populacao: %d\n", Populacao1);
+   printf("Area em KM: %f\n", Area1);
+   printf("PIB: %f\n", PIB1);
+   printf("Numeros de Pontos Turisticos: %d\n", Pontos1);
+printf("---------------------------------------\n");
 
-char Estado2 = B;
-char CódigodaCarta2 = ("B02");
-char NomedaCidade2 = ("Rio de Janeiro");
-int População2 = 6748000;
-float Área2 = 1200.25;
-float PIB2 = 300.50;
-int NúmeroPontos2 = 30
+printf("Carta 2:\n");
+   printf("Estado: %c\n", Estado2); 
+   printf("Codigo: %s\n", Codigo2);
+   printf("Nome da Cidade: %s\n", Cidade2);
+   printf("Populacao: %d\n", Populacao2);
+   printf("Area em KM: %f\n", Area2);
+   printf("PIB: %f\n", PIB2);
+   printf("Numeros de Pontos Turisticos: %d\n", Pontos2);
+printf("---------------------------------------\n");
 
-
-
-
+    return 0;
 }
-
 
 
 
